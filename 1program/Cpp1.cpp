2104be8967f3d0f6a9f6562faf6246612ff739fb
Rustam-aa1 <rustamcaturan75@gmail.com>
2025-10-27 14:12:35 +0100

    Komentarz

---
+++ 1program/Cpp1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "oleg chuesos";
+	return 0;
+}
+
---
+++ 1program/koziawki 2 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int a = 8;
+int b = 4;
+int suma = a + b;
+int roznica = a - b;
+int iloczyn = a * b;
+
+cout << "Suma: " << suma << endl;
+cout << "Roznica: " << roznica << endl;
+cout << "Iloczyn: " << iloczyn << endl;
+return 0;
+}
+	
