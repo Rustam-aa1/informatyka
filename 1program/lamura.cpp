d065e0c8820530f04df2d74b72729325b3e93727
Rustam-aa1 <rustamcaturan75@gmail.com>
2025-11-03 14:10:26 +0100

    Komentarz

---
+++ 1program/lamura.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int punkty;
+    cout << "Podaj liczbe punktow z egzaminu (0-100): ";
+    cin >> punkty;
+
+    if (punkty < 0 || punkty > 100) {
+        cout << "Niepoprawna liczba punktow!" << endl;
+        return 0;
+    }
+double ocena;
+		
+
+
+
+
+    
+
---
+++ 1program/mamura.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int liczba_calkowita = 42;
+    float liczba_zmiennoprzecinkowa = 3.14f;
+    char znak = 'A';
+    bool wartosc_logiczna = true;
+
+    cout << "Typ int: " << liczba_calkowita << endl;
+    cout << "Typ float: " << liczba_zmiennoprzecinkowa << endl;
+    cout << "Typ char: " << znak << endl;
+    cout << "Typ bool: " << wartosc_logiczna << endl;
+
+    return 0;
+}
